e88f04d8b654f6635c7816238f8e7612adc24af8
Harry <congthanh30102001@gmail.com>
2022-10-31 11:13:55 +0700

    cmit

--- AMS_Run.py
+++ AMS_Run.py
@@ -280,7 +280,7 @@ def take_img():
             while (True):
                 ret, img = cam.read()
                 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-                faces = detector.detectMultiScale(gray, 1.3, 5)
+                faces = detector.detectMultiScale(gray, 1.3, 10)
                 for (x, y, w, h) in faces:
                     cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
                     # incrementing sample number
